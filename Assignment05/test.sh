c71136f137ff0427cd4aa68a10063a937aa85a90
jbettini <ashhxtm@gmail.com>
2024-09-13 00:18:46 +0200

    ass05 - add testing script

--- Assignment05/test.sh
+++ Assignment05/test.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-# Ouvre le fichier en lecture
-#exec 3< /dev/fortytwo
+TXT_RED="\033[1;31m"
+TXT_GREEN="\033[1;32m"
+TXT_YELLOW="\033[1;33m"
+TXT_BLUE="\033[1;34m"
+FANCY_RESET="\033[0m"
 
-# Première lecture
-read -n 7 first /dev/fortytwo
+print_color () {
+    echo -e "$1$2$FANCY_RESET"
+}
 
-# Deuxième lecture
-read -n 8 second /dev/fortytwo
+print_color "$TXT_GREEN" "Checking multiple read\n"
 
-# Affiche les résultats
+exec 3< /dev/fortytwo
+
+read -n 7 first <&3
+read -n 8 second <&3
+
 echo "Première lecture : $first"
 echo "Deuxième lecture : $second"
 
-# Ferme le descripteur de fichier
-#exec 3<&-
+exec 3<&-
+
+print_color "$TXT_GREEN" "\nChecking cat\n"
+
+cat /dev/fortytwo
+
+print_color "$TXT_GREEN" "\n\nChecking wrong write\n"
+
+echo "lol" > /dev/fortytwo
+
+print_color "$TXT_GREEN" "\nChecking good write\n"
+
+echo -n "jbettini" > /dev/fortytwo
+
+print_color "$TXT_GREEN" "\nTest finished\n"
